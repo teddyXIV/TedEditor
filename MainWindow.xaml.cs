80fea3073ffe7db9a030ef5a77711f3d8081ac12
Teddy <t.a.peterschmidt@gmail.com>
2024-09-18 14:04:21 -0700

    starting

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace TedEditor;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void NewFile_Click(object sender, RoutedEventArgs e)
+    {
+        tedEditor.Document.Blocks.Clear();
+    }
+
+    private void OpenFile_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void SaveFile_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Exit_Click(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+}
